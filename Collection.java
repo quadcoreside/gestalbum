16370e3d1f8be624320be0aea67dcf96d9ff4683
kolgak <romainpassemard@gmail.com>
2020-11-13 01:35:45 +0100

    l

--- Collection.java
+++ Collection.java
@@ -1,5 +1,17 @@
-class MyClass extends Parent implements Interface {
-  public static void main(String[] args) {
+class Collection {
+String Name;
+String Contenue;
+float Duree;
+}
 
-  }
+class Album extends Collection {
+String Artiste;
+char Genre;
+}
+
+class Playlist extends Collection {
+String Auteur;
+int Langues;
+int Categorie;
+
 }
---
+++ ElementMusical.java
@@ -0,0 +1,17 @@
+class ElementMusical {
+String Name;
+String Contenue;
+float Duree;
+}
+
+class Chanson extends ElementMusical {
+String Artiste;
+char Genre;
+}
+
+class LivreAudio extends ElementMusical {
+String Auteur;
+int Langues;
+int Categorie;
+
+}
